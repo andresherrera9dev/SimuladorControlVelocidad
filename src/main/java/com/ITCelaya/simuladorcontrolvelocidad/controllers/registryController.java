da10b04897a4570d6040d8dbf5c38b040a4f9d0c
andres <23030694@itcelaya.edu.mx>
2025-11-05 15:46:41 -0600

    tableview registry added. Simulation view skeleton added.

--- src/main/java/com/ITCelaya/simuladorcontrolvelocidad/controllers/registryController.java
+++ src/main/java/com/ITCelaya/simuladorcontrolvelocidad/controllers/registryController.java
@@ -1,4 +1,32 @@
 package com.ITCelaya.simuladorcontrolvelocidad.controllers;
 
-public class registryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class registryController implements Initializable {
+    @FXML
+    private TableView<?> tableViewRegistry;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tableViewRegistry.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        tableViewRegistry.widthProperty().addListener((obs, oldWidth, newWidth) -> {
+            double totalWidth = newWidth.doubleValue();
+            int columnCount = tableViewRegistry.getColumns().size();
+
+            if (columnCount > 0) {
+                double adjustedWidth = totalWidth - 20;
+                double columnWidth = adjustedWidth / columnCount;
+                for (TableColumn<?, ?> column : tableViewRegistry.getColumns()) {
+                    column.setPrefWidth(columnWidth);
+                }
+            }
+        });
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.ITCelaya.simuladorcontrolvelocidad {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome6;
     requires mysql.connector.j;
+    requires java.desktop;
     opens com.ITCelaya.simuladorcontrolvelocidad to javafx.fxml;
     exports com.ITCelaya.simuladorcontrolvelocidad;
     exports com.ITCelaya.simuladorcontrolvelocidad.controllers;
